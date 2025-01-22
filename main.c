1d43053d276fb68886d586fbf4ee3a8e7586f1a5
malik <malik@beans.>
2025-01-22 13:25:41 -0500

    fixed a realloc memory issue

--- main.c
+++ main.c
@@ -1,17 +1,22 @@
 #include <stdio.h>
 #include <stddef.h>
+#include <stdlib.h>
+
 #include "p_readline.h"
 
 int
 main()
 {
 	char* lbuf;
-	size_t lbuf_size;
+	size_t lbuf_size = 0;
 	ssize_t len;
 
 	len = p_readline(&lbuf, &lbuf_size);
 
 	printf("allocated a buffer of size %ld and read %ld characters: %s", lbuf_size, len, lbuf);
 
-	return 0;
+	free(lbuf);
+	lbuf = NULL;
+
+	exit(EXIT_SUCCESS);
 }
--- p_readline.c
+++ p_readline.c
@@ -10,8 +10,8 @@ extern ssize_t p_readline(char** line_buf, size_t* lbuf_size)
 	size_t lbuf_pos = 0;
 
 	// malloc an initial buffer for input
-	*line_buf = malloc(BUFFER_SIZE);
 	*lbuf_size = BUFFER_SIZE;
+	*line_buf = (char*) malloc(*lbuf_size);
 	if (*line_buf == NULL)
 	{
 		perror("could not allocate buffer\n");
@@ -39,15 +39,20 @@ extern ssize_t p_readline(char** line_buf, size_t* lbuf_size)
 		}
 
 		// increase buffer size as needed
-		if (lbuf_pos >= *lbuf_size-1) // -1 for string terminator
+		if (lbuf_pos >= *lbuf_size-2) // -1 for string terminator
 		{
-			*line_buf = realloc(*line_buf, *lbuf_size + BUFFER_SIZE);
 			*lbuf_size += BUFFER_SIZE;
-			if (*line_buf == NULL)
+			char* new_lbuf = (char*) realloc(*line_buf, *lbuf_size);
+
+			if (new_lbuf == NULL)
 			{
+				free(*line_buf);
+				*line_buf = NULL;
 				perror("could not reallocate buffer\n");
 				exit(EXIT_FAILURE);
 			}
+
+			*line_buf = new_lbuf;
 		}
 	} while (c > 0);
 
