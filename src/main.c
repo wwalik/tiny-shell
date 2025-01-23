66c8a9fd15ff746fb1306ea0ba29c10f24f6f5fa
w/malik <w.malik@pancakes.live>
2025-01-22 22:58:32 -0500

    wrote the shell skeleton

--- src/main.c
+++ src/main.c
@@ -4,14 +4,35 @@
 
 #include "p_readline.h"
 
+#define PROMPT "$ "
+
+void
+parse_input(char** lbuf, size_t* lbuf_size)
+{
+
+}
+
 int
 main()
 {
 	size_t lbuf_size;
 	char* lbuf;
-	ssize_t len;
+	ssize_t len; 
 
-	len = p_readline(&lbuf, &lbuf_size);
+	while(1)
+	{
+		// DISPLAY PROMPT
+		printf("%s", PROMPT);
+
+		// READ INPUT
+		len = p_readline(&lbuf, &lbuf_size);
+		if (len == EOF)
+			break;
+
+		// PROCESS INPUT
+		parse_input(&lbuf, &lbuf_size);
+	}
+
 
 	printf("allocated a buffer of size %ld and read %ld characters: %s", lbuf_size, len, lbuf);
 
